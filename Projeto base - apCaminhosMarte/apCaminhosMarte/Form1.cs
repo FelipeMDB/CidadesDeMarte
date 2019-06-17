ef68ed644ddaca69c2fba33bac48f434d750251b
FelipeMDB <42344147+FelipeMDB@users.noreply.github.com>
2019-06-17 12:52:07 -0300

    comentei quase tudo

--- Projeto base - apCaminhosMarte/apCaminhosMarte/Form1.cs
+++ Projeto base - apCaminhosMarte/apCaminhosMarte/Form1.cs
@@ -101,14 +101,19 @@ namespace apCaminhosMarte
             BuscarCaminhos(c, idDestino, idOrigem + 1);
         }
 
+        //ao iniciar o programa
         private void Form1_Load(object sender, EventArgs e)
         {
+            //limpamos os itens existentes nos lsbs
             lsbOrigem.Items.Clear();
             lsbDestino.Items.Clear();
+            //instanciamos variáveis
             caminho = new PilhaLista<int>();
             cidades = new Arvore<Cidade>();
-            StreamReader arq = new StreamReader("CidadesMarte.txt");
 
+            //leitura do arquivo de cidades de acordo com o método feito na classe Cidade
+            //incluimos as cidades na árvore de cidades
+            StreamReader arq = new StreamReader("CidadesMarte.txt");
             while (!arq.EndOfStream)
             {
                 Cidade cid = Cidade.LerArquivo(arq);
@@ -116,6 +121,7 @@ namespace apCaminhosMarte
             }
             arq.Close();
 
+            //lemos o arquivo ordenado para inclusão das cidades nos lsbs de origem e destino 
             arq = new StreamReader("CidadesMarteOrdenado.txt");
             while (!arq.EndOfStream)
             {
@@ -125,6 +131,7 @@ namespace apCaminhosMarte
             }
             arq.Close();
 
+            //criamos uma matriz de cidades de acordo com a quantidade de cidades existentes 
             adjacencias = new int[cidades.QuantosDados, cidades.QuantosDados];
             arq = new StreamReader("CaminhosEntreCidadesMarte.txt");
             while (!arq.EndOfStream)
@@ -155,7 +162,8 @@ namespace apCaminhosMarte
 
         }
 
-        private void DesenharArvore(bool primeiraVez, NoArvore<Cidade> raiz,
+        //método para desenhar a árvore de cidades com base no método fornecido a nós pelo professor
+        private void DesenharArvore(bool primeiraVez, NoArvore<Cidade> raiz,     
                            int x, int y, double angulo, double incremento,
                            double comprimento, Graphics g)
         {
@@ -181,6 +189,8 @@ namespace apCaminhosMarte
             }
         }
 
+        //evento que efetivamente chama os métodos que desenham as cidades no mapa e o caminho entre elas
+        //permite a utilização de "Graphics" de pbMapa
         private void pbMapa_Paint(object sender, PaintEventArgs e)
         {
             Graphics gr = e.Graphics;
@@ -190,6 +200,7 @@ namespace apCaminhosMarte
 
         }
 
+        //evento que efetivamente chama o métodos para desenhar a árvore de cidades e nos permite a utilização da classe Graphics necessária para desenhara árvore de cidades
         private void tpArvore_Paint(object sender, PaintEventArgs e)
         {
             Graphics gr = e.Graphics;
@@ -197,57 +208,75 @@ namespace apCaminhosMarte
                                  Math.PI / 2.5, 300, gr);
         }
 
-        private void DesenharCaminho(PilhaLista<int> caminho, Graphics g)
+        private void DesenharCaminho(PilhaLista<int> caminho, Graphics g) //desenha com base em um caminho selecionado nos DGVs ou com base no melhorCaminho
         {
+            //classe que será utilizada para desenhar
             Pen caneta = new Pen(Color.Black);
             caneta.Width = 2.0F;
 
+            //desempilhamos de caminho uma id de uma cidade, verificando sua existência na árvore de cidades
+            // caso exista, a variável cidadeAnterior receberá todas as informações desta cidade
             int idCidadeAnterior = caminho.Desempilhar();
             cidades.Existe(new Cidade(idCidadeAnterior, " ", 0, 0));
             Cidade cidadeAnterior = cidades.Atual.Info;
 
+            //enquanto houver cidades a serem percorridas
             while (!caminho.EstaVazia())
-            {
+            { 
+                //desempilhamos de caminho uma id de uma cidade, verificando sua existência na árvore de cidades
+                // caso exista, a variável cidade receberá todas as informações desta cidade
                 int idCidade = caminho.Desempilhar();
                 cidades.Existe(new Cidade(idCidade, " ", 0, 0));
                 Cidade cidade = cidades.Atual.Info;
 
+                //refazemos as proporções do mapa de acordo com as coordenadas da cidade
                 int x1 = (cidadeAnterior.CoordenadaX * pbMapa.Width) / 4096;
                 int y1 = (cidadeAnterior.CoordenadaY * pbMapa.Height) / 2048;
-
-
+                
+                //refazemos as proporções do mapa de acordo com as coordenadas da cidade
                 int x2 = (cidade.CoordenadaX * pbMapa.Width) / 4096;
                 int y2 = (cidade.CoordenadaY * pbMapa.Height) / 2048;
 
-                g.DrawLine(caneta, x1, y1, x2, y2);
+                //desenhamos uma linha entre as duas cidades, cidadeAnterior representando a posição atual, e cidade representando aonde é necessário ir 
+                g.DrawLine(caneta, x1, y1, x2, y2); //utilizando as localizações "x" e "y" das cidades
+
+                //fazemos com que o destino se torne a origem, para refazer o processo
                 cidadeAnterior = cidade;
             }
         }
 
-        private void dataGridView1_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
-        {
-          //  caminhoASerMostrado = caminhosPossiveis[int.Parse(e.RowIndex.ToString())];
-           // pbMapa.Invalidate();
-        }
 
-        private void ExibirCaminhos()
+        private void ExibirCaminhos() //exibe todos os caminhos possíveis no dataGridView de caminhos 
         {
+            //instanciamos variáveis que serão utilizadas para definir as linhas e colunas do dgv
             int linha = 0;
             int coluna;
-            dataGridView1.RowCount = caminhosPossiveis.Count;
-            dataGridView1.ColumnCount = 1;
+            
+            dataGridView1.RowCount = caminhosPossiveis.Count; //quantidade de linhas é igual à quantidade de caminhos existentes
+            dataGridView1.ColumnCount = 1;// o mesmo será feito com as colunas, mas apenas ao encontrarmos o caminho com a maior quantidade de cidades
+
+            //para cada caminho possível, criaremos clones das pilhas para não estragarmos as originais
             foreach (var caminho in caminhosPossiveis)
             {
+
                 if (caminho.Tamanho() > dataGridView1.ColumnCount)
                     dataGridView1.ColumnCount = caminho.Tamanho();
 
+                //instanciamos uma quantidade de colunas e clonamos o caminho atual para que o mesmo não seja desmontado
                 coluna = 0;
                 var aux = caminho.Clone();
 
+                //desempilhamos a variável auxiliar em outra variável
+                //pois ao desempilhar, invertemos a pilha
+                //ao desempilhar novamente, voltamos a sua forma original
                 PilhaLista<int> aux2 = new PilhaLista<int>();
                 while (!aux.EstaVazia())
                     aux2.Empilhar(aux.Desempilhar());
 
+
+                //enquanto houver cidades a serem mostradas
+                //desempilhamos um código de cidade verificando sua existência na árvore de cidades
+                //em seguida utilizamos a variável atual para recuperar o nome da cidade desempilhada e escrevê-lo na respectiva célula de acordo com "coluna" e "linha"
                 while (!aux2.EstaVazia())
                 {
                     cidades.Existe(new Cidade(aux2.Desempilhar(), "", 0, 0));
@@ -259,19 +288,24 @@ namespace apCaminhosMarte
             }
 
 
-
+            //encontramos o melhorCaminho
             PilhaLista<int> melhorCaminho = SelecionarMelhorCaminho();
             var auxiliar = new PilhaLista<int>();
             
+            //instanciamos as linhas e colunas do dgv
             dataGridView2.RowCount = 1;
             dataGridView2.ColumnCount = melhorCaminho.Tamanho(); 
 
+            //desempilhamos as ids de cidade e as empilhamos em uma variável auxiliar
             while (!melhorCaminho.EstaVazia())
                 auxiliar.Empilhar(melhorCaminho.Desempilhar());
-
+            //enquanto houver cidades a serem mostradas
+            //desempilhamos um código de cidade verificando sua existência na árvore de cidades
+            //em seguida utilizamos a variável atual para recuperar o nome da cidade desempilhada e escrevê-lo na respectiva célula de acordo com sua "coluna"
             coluna = 0;
             while(!auxiliar.EstaVazia())
             {
+
                 cidades.Existe(new Cidade(auxiliar.Desempilhar(), "", 0, 0));
                 dataGridView2.Columns[coluna].HeaderText = "Cidade";
                 dataGridView2.Rows[0].Cells[coluna].Value = cidades.Atual.Info.NomeCidade;
@@ -283,23 +317,31 @@ namespace apCaminhosMarte
 
         private PilhaLista<int> SelecionarMelhorCaminho()   //retorna um inteiro o qual é o índice do melhor caminho guardado para a cidade desejada
         {
-            PilhaLista<int> melhorCaminho = new PilhaLista<int>();
-            int distanciaTotal = 0;
-            int menorDistancia = int.MaxValue;
+            
+            PilhaLista<int> melhorCaminho = new PilhaLista<int>();//criamos uma nova pilha a qual será retornada no final do método contendo o melhor caminho possível
+            int distanciaTotal = 0;                               //distanciaTotal receberá a soma da distância percorrida em um caminho 
+            int menorDistancia = int.MaxValue;                    //menorDistância será usada para definir qual caminho utilizar, comparando as distâncias entre dois caminhos
+
             foreach (var caminho in caminhosPossiveis)
             {
                 distanciaTotal = 0;
                 var aux = caminho.Clone();
-                int anterior = aux.Desempilhar();
+                int anterior = aux.Desempilhar(); //variável que será utilizada como a origem, em qual cidade se está atualmente
 
+                //enquanto ainda houver cidades a serem percorridas
                 while (!aux.EstaVazia())
                 { 
-                    int cidade = aux.Desempilhar();
+                    int cidade = aux.Desempilhar();//desempilharemos o id de uma cidade
+                    //somaremos o valor de distância da cidade que teve o id desempilhado através da matriz adjacências
+                    //que possue como valor a distância a ser percorrida de seu ponto de origem até seu ponto de destino
                     distanciaTotal += adjacencias[anterior, cidade];
-                    anterior = cidade;
+                    anterior = cidade; //fazemos com que a variável de origem receba o destino, de certa forma como se o usuário tivesse percorrido o caminho até a cidade
                 }
 
-                if(menorDistancia > distanciaTotal)
+
+                //se a menorDistancia for maior do que a do caminhoPercorrido
+                //o caminhoPercorrido pelo programa torna-se o melhorCaminho 
+                if (menorDistancia > distanciaTotal) 
                 {
                     menorDistancia = distanciaTotal;
                     melhorCaminho = caminho.Clone();
@@ -309,16 +351,20 @@ namespace apCaminhosMarte
             return melhorCaminho;
         }
 
-        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
+        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e) //quando uma célula específica do dgv receber foco de entrada
         {
-            caminhoASerMostrado = caminhosPossiveis[int.Parse(e.RowIndex.ToString())].Clone();
+            //utilizamos o index da linha para encontrar a qual caminho ela se refere
+            //pois os index do dgv estão de acordo com a List caminhos possíveis
+            caminhoASerMostrado = caminhosPossiveis[int.Parse(e.RowIndex.ToString())].Clone(); 
+            //permissão para redesenhar o mapa
             pbMapa.Invalidate();
         }
 
-        private void dataGridView2_CellEnter(object sender, DataGridViewCellEventArgs e)
-        {
-            caminhoASerMostrado = SelecionarMelhorCaminho();
-            pbMapa.Invalidate();
+        private void dataGridView2_CellEnter(object sender, DataGridViewCellEventArgs e)  //quando uma célula específica do dgv receber foco de entrada
+        { 
+            //este dataGridView mostra epnas o melhor caminho
+            caminhoASerMostrado = SelecionarMelhorCaminho();  //portanto o caminho a ser mostrado recebe a função que acha o melhor caminho
+            pbMapa.Invalidate();                            //permite com que o pbMapa seja redesenhado, chamando o evento paint
         }
     }
 }
