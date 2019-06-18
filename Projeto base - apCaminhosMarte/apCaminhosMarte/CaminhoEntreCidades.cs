984e1157040d521a807e1699995f99125b0fd511
FelipeMDB <42344147+FelipeMDB@users.noreply.github.com>
2019-06-18 16:13:46 -0300

    Update CaminhoEntreCidades.cs

--- Projeto base - apCaminhosMarte/apCaminhosMarte/CaminhoEntreCidades.cs
+++ Projeto base - apCaminhosMarte/apCaminhosMarte/CaminhoEntreCidades.cs
@@ -12,6 +12,7 @@ namespace apCaminhosMarte
 {
     class CaminhoEntreCidades
     {
+        //declaração das variáveis que serão utilizadas para a leitura do arquivo contendo os caminhos entre as cidades 
         int idCidadeOrigem;
         const int inicioIdCidadeOrigem = 0;
         const int tamanhoIdCidadeOrigem = 3;
@@ -32,12 +33,16 @@ namespace apCaminhosMarte
         const int inicioCusto = inicioTempo + tamanhoTempo;
         const int tamanhoCusto = 5;
 
+
+        //getters e setters das variáveis globais da classe
         public int Custo { get => custo; set => custo = value; }
         public int Tempo { get => tempo; set => tempo = value; }
         public int Distancia { get => distancia; set => distancia = value; }
         public int IdCidadeDestino { get => idCidadeDestino; set => idCidadeDestino = value; }
         public int IdCidadeOrigem { get => idCidadeOrigem; set => idCidadeOrigem = value; }
 
+
+        //construtor de um caminho entre duas cidades específicas
         public CaminhoEntreCidades(int idCidOrigem, int idCidDestino, int distancia, int tempo, int custo )
         {
             IdCidadeOrigem = idCidOrigem;
@@ -47,6 +52,7 @@ namespace apCaminhosMarte
             Custo = custo;
         }
 
+        //método que realiza a leitura do arquivo, convertendo strings para as respectivas variáveis globais 
         public static CaminhoEntreCidades LerArquivo(StreamReader arq)
         {
             string linha = arq.ReadLine();
